f09c0195a616775664d564bf4dbfd97389e9d8d8
use <use@DESKTOP-UR90FRE>
2018-03-23 19:08:12 -0400

    team work - test cases 9,10 unfinished

--- src/test/java/com/us_spending/pages/AgencyPage.java
+++ src/test/java/com/us_spending/pages/AgencyPage.java
@@ -17,7 +17,7 @@ public class AgencyPage {
 	private WebDriver driver;
 
 	public AgencyPage() {
-		this.driver=Driver.getDriver();
+		this.driver = Driver.getDriver();
 		PageFactory.initElements(driver, this);
 
 	}
@@ -64,63 +64,70 @@ public class AgencyPage {
 	@FindBy(xpath = "(//table//tbody//tr//td[2]//div//div)")
 	public List<WebElement> budgetaryRecuorcesFormatLlist;// Bobur
 
-	@FindBy(xpath ="//table//thead//td[2]")
+	@FindBy(xpath = "//table//thead//td[2]")
 	public WebElement BudgetaryResourcesClick;
-	
-	@FindBy(xpath ="//button[@class='sort-icon active']")
+
+	@FindBy(xpath = "//button[@class='sort-icon active']")
 	public WebElement BudgetaryResourcesDownLight;
-	
-	@FindBy(xpath ="//button[@class='sort-icon active']")
+
+	@FindBy(xpath = "//button[@class='sort-icon active']")
 	public WebElement BudgetaryResourcesUpLight;
-	
-	@FindBy(xpath ="//table//thead//td[3]")
+
+	@FindBy(xpath = "//table//thead//td[3]")
 	public WebElement persentageButton;
-	
-	@FindBy(xpath ="//button [@class='sort-icon active']")
+
+	@FindBy(xpath = "//button [@class='sort-icon active']")
 	public WebElement persentagelightDown;
-	
-	@FindBy(xpath ="//button [@class='sort-icon active']")
+
+	@FindBy(xpath = "//button [@class='sort-icon active']")
 	public WebElement persentageUpLight;
 
-	@FindBy(xpath ="//button [@class='sort-icon active']")
+	@FindBy(xpath = "//button [@class='sort-icon active']")
 	public WebElement AgencylightDown;
-	
-	@FindBy(xpath ="//button [@class='sort-icon active']")
+
+	@FindBy(xpath = "//button [@class='sort-icon active']")
 	public WebElement AgencyUpLight;
-	
-	@FindBy(xpath ="//table//tbody//td[1]")
+
+	@FindBy(xpath = "//table//tbody//td[1]")
 	public WebElement AdministrativeConferenceButton;
-	   
-	@FindBy(xpath="//p[@class='against-auth-text']")
+
+	@FindBy(xpath = "//p[@class='against-auth-text']")
 	public WebElement ObligatedAmount;
-	
-	@FindBy(xpath="//table//tbody//td[1]")
+
+	@FindBy(xpath = "//table//tbody//td[1]")
 	public List<WebElement> AgencyNames;
-	
-	@FindBy(xpath="//table//tbody//td[2]")
+
+	@FindBy(xpath = "//table//tbody//td[2]")
 	public List<WebElement> AgencyAmount;
-	 
-	public void test()   {
-		  
-		  int row = 0;
-		  for (int i = 0; i < AgencyNames.size(); i++) {
-		   if(AgencyNames.get(i).getText().contains("Department of Housing and Urban Development (HUD)")) {
-		    row = i;
-		   }
-		  }
-		  System.out.println("row= "+row);
-		   
-		   System.out.println(AgencyAmount.get(row).getText());
-	}
-	 
-	
-	@FindBy(xpath=" //a[. = 'Department of Housing and Urban Development (HUD)']")
+
+	@FindBy(xpath = " //a[. = 'Department of Housing and Urban Development (HUD)']")
 	public WebElement DepartmentofHousing;
-	
-	@FindBy(xpath=" //div[@class='authority-amount']")
+
+	@FindBy(xpath = " //div[@class='authority-amount']")
 	public WebElement DepartmentofHousingAmount;
-	
-	
+
+	@FindBy(xpath = "//a[. = 'Department of Agriculture (USDA)']")
+	public WebElement DepartmentofAgriculture;
+
+	@FindBy(xpath = "//a [@class='agency-website']")
+	public WebElement AgencyWebsiteButton;
+
+	@FindBy(xpath = "//table//tbody//td[1]")
+	public List<WebElement> agencyNameList;
+
+	public void test() {
+
+		int row = 0;
+		for (int i = 0; i < AgencyNames.size(); i++) {
+			if (AgencyNames.get(i).getText().contains("Department of Housing and Urban Development (HUD)")) {
+				row = i;
+			}
+		}
+		System.out.println("row= " + row);
+
+		System.out.println(AgencyAmount.get(row).getText());
+	}
+
 	public void clickButton(WebElement element1) {// Bobur
 		element1.click();
 	}
@@ -141,4 +148,23 @@ public class AgencyPage {
 		System.out.printf("Price in USD : %s %n", currencyFormat.format(priceInUSD));
 	}
 
+	public void listToUpperCase() {
+		 List<WebElement> list = agencyNameList;
+		  for (WebElement name : list) {
+		  name.getText().toUpperCase();
+	 }
+	}
+	public void secondList() {
+		for(int i=0; i<agencyNameList.size(); i++) {
+			searchBox.sendKeys(agencyNameList.get(i)); //we need to type the agency names from the list into the Search Box
+		}
+		
+		
+		
+		List<WebElement> list = agencyNameList;
+		for (WebElement name : list) {
+			name.getText().toUpperCase();
+		}
+
+	}
 }
--- src/test/java/com/us_spending/tests/UsSpendingTestCase.java
+++ src/test/java/com/us_spending/tests/UsSpendingTestCase.java
@@ -2,14 +2,16 @@ package com.us_spending.tests;
 
 import static org.testng.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
- 
+
 import com.us_spending.pages.AgencyPage;
 import com.us_spending.pages.HomePage;
 import com.us_spending.pages.PrivacyPage;
@@ -20,18 +22,18 @@ public class UsSpendingTestCase extends TestBaseClass {
 
 	HomePage hp = new HomePage();
 	AgencyPage ap = new AgencyPage();
-	PrivacyPage pp = new PrivacyPage() ;
+	PrivacyPage pp = new PrivacyPage();
 
 	String title = "USAspending.gov";
-	
+
 	String urlHomePage = "https://www.usaspending.gov/#/";
 	String urlAgencyPage = "https://www.usaspending.gov/#/agency";
 	String urlDBpage = "https://www.usaspending.gov/#/db_info";
-	String ppUrl="https://www.usaspending.gov/";
-	
+	String ppUrl = "https://www.usaspending.gov/";
+
 	@Test(priority = 1, description = "USAHM001")
 	public void testCase1() {
-  
+
 		assertTrue(hp.isTitle(title));
 		assertTrue(hp.isUrl(urlHomePage));
 		hp.clickButton(hp.profiles, hp.agencies);
@@ -45,113 +47,147 @@ public class UsSpendingTestCase extends TestBaseClass {
 
 	@Test(priority = 2, description = "USAHM002")
 	public void testCase2() {
-	 	BrowserUtils.waitFor(2);
-	 	assertTrue(hp.isTitle(title));
-	 	assertTrue(hp.isUrl(urlHomePage));
-	 	hp.clickButton(hp.profiles, hp.agencies);
-	 	assertTrue(hp.isUrl(urlAgencyPage));
-	 	ap.search.clear();
-	 	ap.search.sendKeys("babakuly");
-	 	assertTrue(ap.numResult.isDisplayed());
-	 	assertTrue(ap.searchBox.getAttribute("value").equals("babakuly"));
-	 	assertTrue(ap.noResultFound.isDisplayed(), ap.noResultFound.getText());
-	
+		BrowserUtils.waitFor(2);
+		assertTrue(hp.isTitle(title));
+		assertTrue(hp.isUrl(urlHomePage));
+		hp.clickButton(hp.profiles, hp.agencies);
+		assertTrue(hp.isUrl(urlAgencyPage));
+		ap.search.clear();
+		ap.search.sendKeys("babakuly");
+		assertTrue(ap.numResult.isDisplayed());
+		assertTrue(ap.searchBox.getAttribute("value").equals("babakuly"));
+		assertTrue(ap.noResultFound.isDisplayed(), ap.noResultFound.getText());
+
 	}
-	
+
 	@Test(priority = 3, description = "USAHM002")
 	public void testCase3() {
-	 	BrowserUtils.waitFor(2);
-	 	assertTrue(hp.isTitle(title));
-	 	assertTrue(hp.isUrl(urlHomePage));
-	 	hp.clickButton(hp.profiles, hp.agencies);
-	 	assertTrue(hp.isUrl(urlAgencyPage));
-	 	ap.BudgetaryResourcesClick.click();
-	 	assertTrue(ap.BudgetaryResourcesDownLight.isDisplayed());
-	 	ap.BudgetaryResourcesClick.click();
-	 	assertTrue(ap.BudgetaryResourcesUpLight.isDisplayed());
-	 
+		BrowserUtils.waitFor(2);
+		assertTrue(hp.isTitle(title));
+		assertTrue(hp.isUrl(urlHomePage));
+		hp.clickButton(hp.profiles, hp.agencies);
+		assertTrue(hp.isUrl(urlAgencyPage));
+		ap.BudgetaryResourcesClick.click();
+		assertTrue(ap.BudgetaryResourcesDownLight.isDisplayed());
+		ap.BudgetaryResourcesClick.click();
+		assertTrue(ap.BudgetaryResourcesUpLight.isDisplayed());
+
 	}
-	 
+
 	@Test(priority = 4, description = "USAHM002")
 	public void testCase4() {
-	 	BrowserUtils.waitFor(2);
-	 	assertTrue(hp.isTitle(title));
-	 	assertTrue(hp.isUrl(urlHomePage));
-	 	hp.clickButton(hp.profiles, hp.agencies);
-	 	assertTrue(hp.isUrl(urlAgencyPage));
-	 	ap.persentageButton.click();
-	 	assertTrue(ap.persentagelightDown.isDisplayed());
-	 	ap.persentageButton.click();
-	 	assertTrue(ap.persentageUpLight.isDisplayed());
+		BrowserUtils.waitFor(2);
+		assertTrue(hp.isTitle(title));
+		assertTrue(hp.isUrl(urlHomePage));
+		hp.clickButton(hp.profiles, hp.agencies);
+		assertTrue(hp.isUrl(urlAgencyPage));
+		ap.persentageButton.click();
+		assertTrue(ap.persentagelightDown.isDisplayed());
+		ap.persentageButton.click();
+		assertTrue(ap.persentageUpLight.isDisplayed());
 	}
+
 	@Test(priority = 5, description = "USAHM005")
 	public void TestCase5() {
-		
+
 		assertTrue(hp.isUrl(urlHomePage));
 		assertTrue(hp.isTitle(title));
 		hp.clickButton(hp.profiles, hp.agencies);
-		
+
 		assertTrue(ap.isUrl(urlAgencyPage));
 		assertTrue(ap.isTitle(title));
 		ap.clickButton(ap.agencyName);
-	 	assertTrue(ap.AgencylightDown.isDisplayed());
-	 	ap.clickButton(ap.agencyName);
-	 	assertTrue(ap.AgencyUpLight.isDisplayed());
-	
+		assertTrue(ap.AgencylightDown.isDisplayed());
+		ap.clickButton(ap.agencyName);
+		assertTrue(ap.AgencyUpLight.isDisplayed());
+
 	}
-	
+
 	@Test(priority = 6, description = "USAHM006")
 	public void TestCase6() {
 		assertTrue(hp.isUrl(urlHomePage));
 		assertTrue(hp.isTitle(title));
 		hp.clickButton(hp.profiles, hp.agencies);
-		
+
 		assertTrue(ap.isUrl(urlAgencyPage));
 		assertTrue(ap.isTitle(title));
-		
+
 		List<WebElement> list = ap.budgetaryRecuorcesFormatLlist;
 		for (WebElement row : list) {
 			System.out.println(row.getText());
-		//NEW COMMENT
-		     
+			// NEW COMMENT
+
+		}
 	}
-	}
-	 @Test (priority = 8, description = "USAHM008")
+
+	@Test(priority = 8, description = "USAHM008")
 	public void testCase8() throws InterruptedException {
-	 
+
 		assertTrue(hp.isTitle(title));
 		assertTrue(hp.isUrl(urlHomePage));
 		hp.clickButton(hp.profiles, hp.agencies);
 		assertTrue(hp.isUrl(urlAgencyPage));
-		ap.PrivacyPolicy.click();	
+		ap.PrivacyPolicy.click();
 		BrowserUtils.waitFor(3);
 		assertTrue(ap.isTitle(title));
 		BrowserUtils.waitForPageToLoad(2);
 		assertTrue(driver.getCurrentUrl().contains(ppUrl));
-		
+
 		assertTrue(pp.Privacytop.getText().contains("Legal"));
 		assertTrue(pp.PrivacyPolicy.getText().equals("Privacy Policy"));
 
 	}
-	
-	@Test(priority = 11, description = "USRID011")
-	public void testCase11() {	
- 
+
+	@Test (priority = 9, description = "USAHM009")
+		public void testCase9() throws InterruptedException {
+		 
+			assertTrue(hp.isTitle(title));
+			assertTrue(hp.isUrl(urlHomePage));
+			hp.clickButton(hp.profiles, hp.agencies);
+			
+			ap.listToUpperCase();
+			
+			
+	}
+			
+
+	@Test(priority = 10, description = "USRID011")
+	public void testCase10() {
+
 		assertTrue(hp.isTitle(title));
 		assertTrue(hp.isUrl(urlHomePage));
-		 
+
+		hp.clickButton(hp.profiles, hp.agencies);
+		ap.DepartmentofAgriculture.click();
+		BrowserUtils.waitFor(2);
+		ap.AgencyWebsiteButton.click();
+
+		driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "\t");
+
+		System.out.println(driver.getTitle());
+
+		assertTrue(driver.getTitle().equals("USDA"));
+		assertTrue(driver.getCurrentUrl().equals("https://www.usda.gov/"));
+	}
+
+	@Test(priority = 11, description = "USRID011")
+	public void testCase11() {
+
+		assertTrue(hp.isTitle(title));
+		assertTrue(hp.isUrl(urlHomePage));
+
 		hp.clickButton(hp.profiles, hp.agencies);
 	}
 
-	@Test(priority = 12, description = "USRID012") 
+	@Test(priority = 12, description = "USRID012")
 	public void testCase12() {
 		driver.manage().window().maximize();
-		assertTrue(hp.isTitle(title));		
+		assertTrue(hp.isTitle(title));
 		hp.spendEx.click();
 		assertTrue(hp.spendEx.isDisplayed());
-		
 
 	}
+
 	@Test(priority = 13, description = "USAHM013")
 	public void testCase13() {
 		BrowserUtils.waitFor(2);
@@ -162,9 +198,9 @@ public class UsSpendingTestCase extends TestBaseClass {
 		BrowserUtils.waitFor(2);
 		assertTrue(hp.AdvanceSearch.isDisplayed());
 		assertTrue(hp.KeywordSearch.isDisplayed());
-		
+
 	}
-	
+
 	@Test(priority = 14, description = "USAHM014")
 	public void testCase14() {
 		BrowserUtils.waitFor(2);
@@ -176,7 +212,7 @@ public class UsSpendingTestCase extends TestBaseClass {
 		assertTrue(hp.profiles.isDisplayed());
 		assertTrue(hp.FedaralAgencies.isDisplayed());
 	}
-	
+
 	@Test(priority = 15, description = "USAHM015")
 	public void testCase15() {
 		BrowserUtils.waitFor(2);
@@ -190,28 +226,28 @@ public class UsSpendingTestCase extends TestBaseClass {
 		assertTrue(hp.btnDataBaseSnapshots.isDisplayed());
 		assertTrue(hp.btnAPI.isDisplayed());
 	}
+
 	@Test(priority = 16, description = "USAHM016")
 	public void testCase16() {
 		BrowserUtils.waitFor(2);
 		assertTrue(hp.isUrl(urlHomePage));
-	
+
 		hp.Glossary.click();
 		BrowserUtils.waitFor(2);
 		assertTrue(hp.GlossarySearchPlace.isDisplayed());
 	}
-	
+
 	@Test(priority = 17, description = "USRID011")
-	public void testCase17() {	
+	public void testCase17() {
 		assertTrue(hp.isTitle(title));
 		assertTrue(hp.isUrl(urlHomePage));
 		hp.clickButton(hp.profiles, hp.agencies);
 		BrowserUtils.waitForPageToLoad(2);
 		ap.search.sendKeys("administrative");
 		ap.AdministrativeConferenceButton.click();
- 
-		 
+
 	}
- 
+
 	@Test(priority = 19) //// USILY 019 MARIA DOBROKHODOVA
 	public void signIn() {
 		assertTrue(hp.isTitle(title));
@@ -221,31 +257,30 @@ public class UsSpendingTestCase extends TestBaseClass {
 		System.out.println(hp.departmentName.getText());
 		System.out.println(hp.departmentBudget.getText());
 		ap.DepartmentofHousing.click();
-		
+
 		System.out.println(hp.departmentName2.getText());
-		
+
 		System.out.println(hp.departmentBudget2.getText());
-//		assertTrue(hp.departmentName.getText().equals(hp.departmentName2.getText()));
-//		assertTrue(hp.departmentBudget.getText().equals(hp.departmentBudget2.getText()));
+		// assertTrue(hp.departmentName.getText().equals(hp.departmentName2.getText()));
+		// assertTrue(hp.departmentBudget.getText().equals(hp.departmentBudget2.getText()));
 	}
-	
+
 	@Test(priority = 21, description = "USAHM0021")
 	public void testCase7() {
-		  
+
 		BrowserUtils.waitForPageToLoad(2);
 		assertTrue(hp.isTitle(title));
 		assertTrue(hp.isUrl(urlHomePage));
 		hp.clickButton(hp.profiles, hp.agencies);
-		assertTrue(hp.isUrl(urlAgencyPage));	
+		assertTrue(hp.isUrl(urlAgencyPage));
 		ap.search.sendKeys("");
 		BrowserUtils.scroll(ap.ClickHere);
 		ap.ClickHere.click();
 		BrowserUtils.waitFor(3);
 		BrowserUtils.switchToWindowUrl(urlDBpage);
-		assertTrue(hp.isTitle(title));	
+		assertTrue(hp.isTitle(title));
 		BrowserUtils.waitForPageToLoad(2);
 		assertTrue(hp.isUrl(urlDBpage), driver.getCurrentUrl());
-		
-		     
-	} 
+
+	}
 }
